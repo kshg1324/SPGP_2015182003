fc519ee7d6000280a1156029e8e43ce9c2f373a2
kshg1324(laptop) <kshg1324@naver.com>
2021-03-26 12:23:52 +0900

    TextView와 Rectangle에 margin 및 padding값 추가, ontouchevent 관찰

--- 4. CustomControls/app/src/main/java/kr/ac/kpu/game/s2015182003/customcontrols/MainActivity.java
+++ 4. CustomControls/app/src/main/java/kr/ac/kpu/game/s2015182003/customcontrols/MainActivity.java
@@ -3,15 +3,18 @@ package kr.ac.kpu.game.s2015182003.customcontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        MyView mv = new MyView(this);
-        setContentView(mv);
+//        MyView mv = new MyView(this,);
+//        setContentView(mv);
     }
+
 }
