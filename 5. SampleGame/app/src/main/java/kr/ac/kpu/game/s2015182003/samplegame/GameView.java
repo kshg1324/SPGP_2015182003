b7d054bab60bdf3cb19408654637357a1deb9fcd
kshg1324(laptop) <kshg1324@naver.com>
2021-04-02 11:59:21 +0900

    플레이어(비행기) 생성 및 배치 그리고 터치와 드래그로 이동할 수 있게 함

--- 5. SampleGame/app/src/main/java/kr/ac/kpu/game/s2015182003/samplegame/GameView.java
+++ 5. SampleGame/app/src/main/java/kr/ac/kpu/game/s2015182003/samplegame/GameView.java
@@ -9,16 +9,22 @@ import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Choreographer;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameView extends View
 {
     private static final String TAG = GameView.class.getSimpleName();
+    public static final int BALL_COUNT = 10;
 
+    Player player;
+    ArrayList<Ball> balls = new ArrayList<>();
     private Ball b1;
     private Ball b2;
 
@@ -42,8 +48,12 @@ public class GameView extends View
 
     private void doGameFrame()
     {
-        b1.update();
-        b2.update();
+        for(Ball b: balls)
+        {
+            b.update();
+        }
+     //   b1.update();
+     //   b2.update();
 
      //   b1.x += b1.dx * frameTime;
      //   b1.y += b1.dy * frameTime;
@@ -67,17 +77,44 @@ public class GameView extends View
         });
     }
     private void initResource() {
-        b1 = new Ball(100, 100, 100, 200);
-        b2 = new Ball(900, 100, -50, 150);
+        player = new Player(100, 100, 0, 0);
+        Random rand = new Random();
+        for(int i = 0; i < BALL_COUNT; ++i)
+        {
+            float x = rand.nextInt(1000);
+            float y = rand.nextInt(1000);
+            float dx = rand.nextFloat() * 1000 - 500;
+            float dy = rand.nextFloat() * 1000 - 500;
+            Ball b = new Ball(x, y, dx, dy);
+            balls.add(b);
+        }
+        //b1 = new Ball(100, 100, 100, 200);
+        //b2 = new Ball(900, 100, -50, 150);
     }
 
     @Override
     protected void onDraw(Canvas canvas)
     {
-        b1.draw(canvas);
-        b2.draw(canvas);
+        for(Ball b: balls)
+        {
+            b.draw(canvas);
+        }
+        player.draw(canvas);
+        //b1.draw(canvas);
+        //b2.draw(canvas);
         //canvas.drawBitmap(bitmap, b1.x, b1.y,null);
         //canvas.drawBitmap(bitmap, b2.x, b2.y,null);
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE)
+        {
+            player.moveTo(event.getX(), event.getY());
+            return true;
+        }
+        return false;
     }
 }
---
+++ 5. SampleGame/app/src/main/java/kr/ac/kpu/game/s2015182003/samplegame/Player.java
@@ -0,0 +1,57 @@
+package kr.ac.kpu.game.s2015182003.samplegame;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+public class Player {
+    private static int imagewidth;
+    private static int imageheight;
+    private float x, y;
+    private float dx, dy;
+    private static Bitmap bitmap;
+
+    public Player(float x, float y, float dx, float dy)
+    {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        if(bitmap == null) {
+            Resources res = GameView.view.getResources();
+            bitmap = BitmapFactory.decodeResource(res, R.mipmap.plane_240);
+            imagewidth = bitmap.getWidth();
+            imageheight = bitmap.getHeight();
+        }
+    }
+
+    public void moveTo(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void update()
+    {
+     //   this.x += this.dx * GameView.frameTime;
+     //   this.y += this.dy * GameView.frameTime;
+     //   int w = GameView.view.getWidth();
+     //   int h = GameView.view.getHeight();
+     //   if(x < 0 || x > w - imagewidth)
+     //   {
+     //       dx *= -1;
+     //   }
+//
+     //   if(y < 0 || y > h - imageheight)
+     //   {
+     //       dy *= -1;
+     //   }
+    }
+
+    public void draw(Canvas canvas)
+    {
+        float left = x - imagewidth / 2;
+        float top = y - imageheight / 2;
+        canvas.drawBitmap(bitmap, left, top, null);
+    }
+}
